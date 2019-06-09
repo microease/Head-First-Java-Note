427c38eb8e884357c24652f3f07b0eb678b4ca92
microease <microease@163.com>
2019-06-10 01:03:15 +0800

    add

---
+++ Chapter_1/demo01.java
@@ -0,0 +1,9 @@
+public class demo01 {
+    public static void main(String[] args) {
+        int size = 27;
+        String name = "Fido";
+        Dog myDog = new Dog(name,size);
+        x = size -5;
+        if(x<15) myDog.bark(8);
+    }
+}
